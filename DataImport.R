05f04f7f98469a9fe78809861097d975bb98124d
tpham45 <tpham45@uco.edu>
2023-10-31 14:07:03 -0500

    First Update

---
+++ DataImport.R
@@ -0,0 +1,52 @@
+library(AzureStor)
+
+--------------------------------------------------------------------------------------------------------
+#Create Token:
+# Acquiring an Azure token for secure and temporary authentication to Azure services.
+# Tokens ensure safe access without exposing main credentials and can be scoped for specific permissions.
+  
+token <- AzureRMR::get_azure_token ("https://storage.azure.com"
+                  ,tenant="31ec2513-6818-4351-bfdf-52eda65b2f71"
+                  ,app="1e1bc807-4976-4770-b9bb-10462fe37f13"
+                  ,password="NSZ8Q~4L8eOXJp8J9QKZASW.Z2ou~cwC9rzBUaTR"
+                   )
+---------------------------------------------------------------------------------------------------------
+#Create Endpoint Token:
+# Set up an endpoint to connect to the 'tokyoplympicsdata' Azure Blob storage.
+# Use the previously obtained authentication token for secure access.
+ad_endp_tok <- storage_endpoint("https://tokyoplympicsdata.blob.core.windows.net/",token=token)
+---------------------------------------------------------------------------------------------------------
+# Connect to the 'tokyodatasources' container within the Azure Blob storage endpoint.
+# List all files within the 'transformed-data' directory of the container.
+cont <- storage_container(ad_endp_tok, "tokyodatasources")
+list_storage_files(cont,"transformed-data")
+---------------------------------------------------------------------------------------------------------
+#Athletes Data Import:
+storage_download(cont, "transformed-data/athletes/athletes_fd.csv", "athletes_fd.csv", overwrite = TRUE)
+athletes = read.csv("athletes_fd.csv")
+print (athletes)
+
+#Coaches Data Import:
+storage_download(cont, "transformed-data/coaches/coach_fd.csv", "coach_fd.csv", overwrite = TRUE)
+coaches = read.csv("coach_fd.csv")
+print (coaches)
+
+#Entriesgender Data Import:
+storage_download(cont, "transformed-data/entriesgender/entriesgen_fd.csv", "entriesgen_fd.csv", overwrite = TRUE)
+entriesgender = read.csv("entriesgen_fd.csv")
+print (entriesgender)
+
+#Medals Data Import:
+storage_download(cont, "transformed-data/medals/medal_fd.csv", "medal_fd.csv", overwrite = TRUE)
+medals = read.csv("medal_fd.csv")
+print (medals)
+
+#Teams Data Import:
+storage_download(cont, "transformed-data/teams/team_fd.csv", "team_fd.csv", overwrite = TRUE)
+teams = read.csv("team_fd.csv")
+print (teams)
+
+#AthletesMed Data Import:
+storage_download(cont, "transformed-data/athletesmedal/athletesmd_fd.csv", "athletesmd_fd.csv", overwrite = TRUE)
+athmedals = read.csv("athletesmd_fd.csv")
+print (athmedals)
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Data-Engineer-Project
+Introduction
+* As a Data Engineer. My mission is giving the best dataset for furthure advanced analytics.
+* As a Data Scintiest. My mission is applying statistical skill to analyze data in advance and giving best prediction for decision-making.
+
+Preview: This project is a combination between DE and DS workskill. I have applied framework bases on Azure for ETL methodology. Then using the dataset after cleaning and transforming, I have turned them into R for DS analyzing futhure.
+
+Tool:
+* Azure Data Factory - Data Intergation 
+* Data Lake Gen 2 - Raw & Transformed Data Store
+* Azure Databricks - Data Transformations
+* Azure Synapse Analytics - Advanced Analytics
+* R Studio - DS Advanced Analytics
+
+Framework:
+"![Alt text](image.png)"
+
+Instruction:
+* Step 1: Data Set Up
+    - Download Dataset from Kaggle.
+    - Store data on Github Repos: 'https://github.com/tpham45/Data-Engineer-Project.git'.
+
+* Step 2: Data Extract
+    - Data Factory - Intergate data from data source (Github)
+
+
+
+References:
+* Dataset: 'https://www.kaggle.com/datasets/arjunprasadsarkhel/2021-olympics-in-tokyo/code'
+* Instruction Video: 'https://www.youtube.com/watch?v=IaA9YNlg5hM' 
+
